176f532cc2c3adbef99c99ea5890fac8ff2f6cc3
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2016-04-01 03:19:21 +0200

    Handle back/forward navigation with dialogs

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -33,3 +33,43 @@ window.requestIdleCallback =
         () => reject());
     });
   }
+
+  var _lastState = history.state;
+
+  function _replaceState() {
+     console.log("replace undefined");
+     _lastState = {};
+     history.replaceState(_lastState, window.location.href);
+  }
+
+  function pushOpenedState(dialog) {
+      console.log("push " + dialog.nodeName.toLowerCase());
+      dialog.addEventListener('iron-overlay-canceled', _replaceState);
+
+      _lastState = { id: dialog.nodeName.toLowerCase() };
+      history.pushState( _lastState, window.location.href);
+  }
+
+  function onpopstate(event) {
+      console.log("pop " + (event.state) ? event.state.id : undefined);
+      let pendingOpen = event.state ? event.state.id : undefined;
+      let pendingClose = _lastState ? _lastState.id : undefined;
+
+      if (pendingClose) {
+        let dialog = document.querySelector(pendingClose);
+        if (dialog && dialog.opened) {
+            dialog.close();
+        }
+      }
+
+      if (pendingOpen) {
+        let dialog = document.querySelector(pendingOpen);
+        if (dialog && !dialog.opened) {
+           dialog.open();
+        }
+      }
+
+      _lastState = event.state;
+  }
+
+  window.addEventListener('popstate', onpopstate);
