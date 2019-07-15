c267edca27d7dcc16e9785991e61fdded065969e
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2019-07-15 16:09:37 +0200

    Readd NFC support

--- src/groceries-list.js
+++ src/groceries-list.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css, property, customElement } from 'lit-element';
 import { nothing } from 'lit-html';
 import { repeat } from 'lit-html/directives/repeat.js';
+import { classMap } from 'lit-html/directives/class-map.js'
 import "@material/mwc-checkbox";
 
 import { style as listStyle } from './mwc-list-item-css.js';
@@ -50,28 +51,46 @@ export class GroceriesList extends LitElement {
       width: 100%;
       background-color: yellow;
     }
+
+    .hidden {
+        display: none;
+      }
   `];
 
   store = new GroceryStore;
-  items = [];
+  pendingItems = [];
+  doneItems = [];
 
   constructor() {
     super();
     const onchange = async () => {
-      let notDone = [];
-      let done = [];
+      this.doneItems = [];
+      this.pendingItems = [];
       for await (let entry of this.store.entries()) {
         if (entry.done) {
-          done.push(entry);
+          this.doneItems.push(entry);
         } else {
-          notDone.push(entry);
+          this.pendingItems.push(entry);
         }
       }
-      this.items = notDone.concat(done);
       await this.requestUpdate();
     };
     this.store.addEventListener('change', onchange);
     onchange();
+
+    try {
+      const reader = new NFCReader({url: document.baseURI, recordType: "json"});
+      reader.addEventListener("reading", event => {
+        for (let record of event.message.records) {
+          if (record.data.product) {
+            this.store.set(record.data.product, record.data.description);
+          }
+        }
+      });
+      reader.start();
+    } catch(err) {
+      console.error("Reading NFC tags is not supported");
+    }
   }
 
   onchange(ev) {
@@ -82,25 +101,25 @@ export class GroceriesList extends LitElement {
   render() {
     return html`
       <div role="list" class="mdc-list mdc-list--two-line">
-        ${repeat(this.items, v => v.name, v => {
-          return !v.done ? html`
+        ${repeat(this.pendingItems, v => v.name, v => {
+          return html`
             <grocery-item
               .label=${v.name}
               .sublabel=${v.note}
               @change=${this.onchange}></grocery-item>
-          ` : nothing;
+          `;
         })}
       </div>
-      <hr>
+      <hr class=${classMap({hidden: !this.doneItems.length})}>
       <div role="list" class="mdc-list mdc-list--two-line">
-        ${repeat(this.items, v => v.name, v => {
-          return v.done ? html`
+        ${repeat(this.doneItems, v => v.name, v => {
+          return html`
             <grocery-item
               .label=${v.name}
               .sublabel=${v.note}
               checked
               @change=${this.onchange}></grocery-item>
-          ` : nothing;
+          `;
         })}
       </div>
     `;
--- src/main-app.js
+++ src/main-app.js
@@ -1,9 +1,12 @@
 
 import { LitElement, html, css, property, customElement } from 'lit-element';
+import "@material/mwc-checkbox";
 import "@material/mwc-drawer";
 import "@material/mwc-fab";
+import "@material/mwc-formfield";
 import "@material/mwc-top-app-bar";
 import "@material/mwc-icon-button";
+import "@material/mwc-snackbar";
 import "@authentic/mwc-dialog/mwc-dialog";
 import "@authentic/mwc-textfield/mwc-textfield";
 
@@ -11,13 +14,110 @@ import { query } from '@material/mwc-base/base-element.js';
 
 import "./groceries-list.js";
 import { GroceryStore } from './grocery-store.js';
-import { Dialog } from '@authentic/mwc-dialog/mwc-dialog';
 
+@customElement('add-dialog')
+export class AddDialog extends LitElement {
+  store = new GroceryStore;
+
+  @query('mwc-dialog') dialog;
+  // TODO: use classMap to hide checkbox when !NFCWriter in window
+  @query('mwc-checkbox') checkbox;
+  @query('mwc-snackbar') snackbar;
+  @query('#actionButton') actionBtn;
+  @query('#product') product;
+  @query('#description') description;
+
+  firstUpdated() {
+    this.dialog.addEventListener('MDCDialog:closed', async (e) => {
+      if (e.detail.action !== "accept") return;
+
+      const writeToNFC = this.checkbox.checked;
+      if (writeToNFC) {
+        this.writeToNFC(this.product.value, this.description.value);
+      } else {
+        this.store.set(this.product.value, this.description.value);
+      }
+    });
+  }
+
+  async writeToNFC(product, description) {
+    const ndef = {
+      records: [{
+        recordType: "json",
+        mediaType: "application/json",
+        data: { product, description }
+      }]
+    };
+
+    try {
+      const controller = new AbortController;
+      this.snackbar.addEventListener('MDCSnackbar:closed', (ev) => {
+        if (ev.detail.reason === "action") {
+          controller.abort();
+        }
+      }, { once: true });
+
+      this.snackbar.labelText = "Touch your NFC tag now";
+      this.actionBtn.textContent = "CANCEL";
+      this.snackbar.open();
+      const writer = new NFCWriter();
+      await writer.push(ndef, {
+        target: "tag",
+        ignoreRead: true,
+        compatibility: "any",
+        signal: controller.signal
+      });
+      this.snackbar.close();
+    } catch (err) {
+      this.snackbar.close();
+
+      this.snackbar.addEventListener('MDCSnackbar:closed', (ev) => {
+        if (ev.detail.reason === "action") {
+          Promise.resolve().then(() => this.writeToNFC(product, description));
+        }
+      }, { once: true });
+      this.snackbar.labelText = `Writing failed: ${err}`;
+      this.actionBtn.textContent = "RETRY";
+      this.snackbar.open();
+    }
+  }
+
+  open() {
+    let options = ["Milk", "Cheese", "Beer", "Cocoa", "Candy"];
+    let entry = options[Math.floor(Math.random() * options.length)];
+    this.description.value = "";
+    this.checkbox.checked = false;
+    this.product.value = entry;
+    this.dialog.open();
+  }
+
+  render() {
+    return html`
+      <mwc-dialog id="dialog" headerLabel="Add new item" acceptLabel="Add" declineLabel="Cancel">
+        <mwc-textfield outlined required id="product"
+          type="text"
+          label="Enter product..."
+          value="Milk"
+          helperText="Add a product like milk, bananas, etc">
+        </mwc-textfield>
+        <mwc-textfield outlined id="description"
+          type="text"
+          label="Enter description...">
+        </mwc-textfield>
+        <mwc-formfield label="Write to NFC tag instead">
+          <mwc-checkbox></mwc-checkbox>
+        </mwc-formfield>
+      </mwc-dialog>
+      <mwc-snackbar stacked>
+        <mwc-button id="actionButton" slot="action">CANCEL</mwc-button>
+        <mwc-icon-button id="iconButton" icon="close" slot="dismiss"></mwc-icon-button>
+      </mwc-snackbar>
+    `;
+  }
+}
 
 @customElement('main-app')
 export class MainApplication extends LitElement {
-  @property() name = 'World';
-
   static styles = css`
     .drawer-content {
       padding: 0px 16px 0 16px;
@@ -36,10 +136,7 @@ export class MainApplication extends LitElement {
     }
   `;
 
-  store = new GroceryStore;
-  @query('mwc-dialog') dialog;
-  @query('#product') product;
-  @query('#description') description;
+  @query('add-dialog') dialog;
 
   firstUpdated() {
     const drawer = this.shadowRoot.querySelector('mwc-drawer');
@@ -47,28 +144,15 @@ export class MainApplication extends LitElement {
     container.addEventListener('MDCTopAppBar:nav', _ => {
       drawer.open = !drawer.open;
     });
-    this.dialog.addEventListener('MDCDialog:closed', async (e) => {
-      if (e.detail.action !== "accept") return;
-      this.store.set(this.product.value, this.description.value);
-    });
-  }
-
-  async onnew() {
-    let options = ["Milk", "Cheese", "Beer", "Cocoa", "Candy"];
-    let entry = options[Math.floor(Math.random() * options.length)];
-    this.description.value = "";
-    this.product.value = entry;
-    this.dialog.open();
   }
 
   render() {
     return html`
       <mwc-drawer hasHeader type=modal>
-        <span slot="title">Drawer Title</span>
-        <span slot="subtitle">subtitle</span>
+        <span slot="title">Web NFC Grocery Demo</span>
+        <span slot="subtitle">A demonstration of Web NFC</span>
         <div class="drawer-content">
-          <p>Drawer content</p>
-          <mwc-icon-button icon="gesture"></mwc-icon-button>
+          <p><a href="https://w3c.github.io/web-nfc/">Web NFC API specification</a></p>
         </div>
         <div slot="appContent">
           <mwc-top-app-bar>
@@ -80,19 +164,8 @@ export class MainApplication extends LitElement {
           </div>
         </div>
       </mwc-drawer>
-      <mwc-fab icon="playlist_add" @click=${this.onnew}></mwc-fab>
-      <mwc-dialog id="dialog" headerLabel="Add new item" acceptLabel="Add" declineLabel="Cancel">
-        <mwc-textfield outlined required id="product"
-          type="text"
-          label="Enter product..."
-          value="Milk"
-          helperText="Add a product like milk, bananas, etc">
-        </mwc-textfield>
-        <mwc-textfield outlined id="description"
-          type="text"
-          label="Enter description...">
-        </mwc-textfield>
-      <mwc-dialog>
+      <mwc-fab icon="playlist_add" @click=${() => this.dialog.open()}></mwc-fab>
+      <add-dialog></add-dialog>
     `;
   }
 }
