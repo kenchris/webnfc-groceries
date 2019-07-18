969ba3b15496e1d7aad6c857fc9725b6881a1f29
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2019-07-18 12:00:45 +0200

    Add half working SW

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,32 @@
 import createDefaultConfig from '@open-wc/building-rollup/modern-config';
+import workbox from 'rollup-plugin-workbox/dist/index.js';
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import createDefaultConfig from '@open-wc/building-rollup/modern-and-legacy-config';
 
-export default createDefaultConfig({
-    input: './index.html',
-    extensions: ['.js', '.mjs'],
-  });
+let config = createDefaultConfig({
+  input: './index.html',
+  extensions: ['.js', '.mjs']
+});
+
+const plugins = [
+  workbox({
+    mode: 'generateSW',
+    render: ({ swDest, count, size }) => console.log(
+      'dest', swDest,
+      '#️', count,
+      'size', size,
+    ),
+    workboxConfig: {
+      "globDirectory": "dist",
+      "globPatterns": [
+        '**/*.{js,css,html}'
+      ],
+      "swDest": "dist/sw.js"
+    },
+  })
+];
+
+config.plugins = config.plugins.concat(plugins);
+
+export default config;
