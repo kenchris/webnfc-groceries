84abcb5a0fdc830990d29ddec0123c2d1119ba46
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2019-07-18 12:53:04 +0200

    Make my own workbox plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import createDefaultConfig from '@open-wc/building-rollup/modern-config';
-import workbox from 'rollup-plugin-workbox/dist/index.js';
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import createDefaultConfig from '@open-wc/building-rollup/modern-and-legacy-config';
@@ -9,22 +8,25 @@ let config = createDefaultConfig({
   extensions: ['.js', '.mjs']
 });
 
+function workbox(config) {
+	return {
+		name: 'workbox',
+		async writeBundle() {
+      let build = require('workbox-build');
+      const { count, size } = await build.generateSW(config);
+      console.log(count, size);
+    }
+	};
+}
+
 const plugins = [
   workbox({
-    mode: 'generateSW',
-    render: ({ swDest, count, size }) => console.log(
-      'dest', swDest,
-      '#️', count,
-      'size', size,
-    ),
-    workboxConfig: {
-      "globDirectory": "dist",
-      "globPatterns": [
-        '**/*.{js,css,html}'
-      ],
-      "swDest": "dist/sw.js"
-    },
-  })
+    "globDirectory": "dist",
+    "globPatterns": [
+      '**/*.{js,css,html}'
+    ],
+    "swDest": "dist/sw.js"
+  }),
 ];
 
 config.plugins = config.plugins.concat(plugins);
