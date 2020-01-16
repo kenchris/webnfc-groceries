e0c7ffadcd32c611c329e7fae78e8510782c7aaa
Wanming Lin <wanming.lin@intel.com>
2020-01-16 10:30:12 +0800

    New deploy

--- docs/sw.js
+++ docs/sw.js
@@ -67,11 +67,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "059c5a42958f91bdcdf4ccd4933aedbb"
+    "revision": "05490c16f80faa6abde8b7a90f2ef81b"
   },
   {
-    "url": "main-app-9d4578f5.js",
-    "revision": "e0fc9989993f2b5f642d06e83b5b7cb2"
+    "url": "main-app-926b2732.js",
+    "revision": "9a894799940e0189d85354f40b1d31e7"
   },
   {
     "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",
@@ -82,8 +82,8 @@ self.__precacheManifest = [
     "revision": "ed55766050be285197b8f511eacedb62"
   },
   {
-    "url": "polyfills/webcomponents.dae9f79d9d6992b6582e204c3dd953d3.js",
-    "revision": "fe4a22f36087db029cd3f476a1935410"
+    "url": "polyfills/webcomponents.cc3976af76b5e726e2a7e86686c930f1.js",
+    "revision": "6d41a7c92156aec243e9ce5680bdf517"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
